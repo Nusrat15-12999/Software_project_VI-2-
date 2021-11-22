a1a2fa6a6b2edf0dab23d4b0af769d22adbd2124
Nusrat15-12999 <nusrat15-12999@diu.edu.bd>
2021-11-22 17:36:11 +0600

    Version-1

---
+++ 4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+
+    int i, j, len, v = 1;
+    char s[100];
+
+    printf("Enter a string:");
+    gets(s);
+
+    len = strlen(s);
+
+    for (i = 0; i < len; i++) {
+        for (j = i + 1; j < len; j++) {
+            if (s[i] == s[j]) {
+                if (v == 1 && s[j] != '\0') {
+                    printf("%c", s[i]);
+                }
+                s[j] = '\0';
+                v++;
+            }
+        }
+        v = 1;
+    }
+
+    return 0;
+}
